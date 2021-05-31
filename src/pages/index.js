a4b35916c3268da36773996ff5397743d68f9481
victor <v.degermain@gmail.com>
2021-05-31 10:59:39 +0200

    modification theme css

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,12 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { FaGithub, FaBolt, FaHome, FaWrench } from 'react-icons/fa'
 import styled from 'styled-components'
+import  '../assets/css/css/bootstrap.min.css'
+import  '../assets/css/css/style.css'
+import  '../assets/css/css/site.css'
+import  '../assets/css/css/main.css'
+import  '../assets/css/css/dark-theme.css'
+import  '../assets/css/css/print.css'
 
 let StyledBackground = styled.div`
   background: linear-gradient(to bottom,#598fc6 0,#fff 100%);
